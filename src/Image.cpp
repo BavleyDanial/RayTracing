1ba202a7c69ea9f81ef0a394559d1d729fd1d0ec
PavlyDev <bavleydanial@gmail.com>
2025-01-18 22:14:33 +1100

    Added diffused lighting

--- src/Image.cpp
+++ src/Image.cpp
@@ -3,10 +3,15 @@
 namespace RT {
 
     void DrawPixel(Image& image, int pixelPos, const glm::vec4& color) {
-        image.pixels[pixelPos] = color.r;
-        image.pixels[pixelPos + 1] = color.g;
-        image.pixels[pixelPos + 2] = color.b;
-        image.pixels[pixelPos + 3] = color.a;
+        uint8_t r = static_cast<uint8_t>(color.r);
+        uint8_t g = static_cast<uint8_t>(color.g);
+        uint8_t b = static_cast<uint8_t>(color.b);
+        uint8_t a = static_cast<uint8_t>(color.a);
+
+        image.pixels[pixelPos] = r;
+        image.pixels[pixelPos + 1] = g;
+        image.pixels[pixelPos + 2] = b;
+        image.pixels[pixelPos + 3] = a;
     }
 
     void DrawPixel(Image& image, int pixelPos, const glm::vec3& color) {
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
+#include <stdio.h>
 
 #include <Image.h>
 #include <ImageFile.h>
 
 #include <glm/glm.hpp>
 
+#define LOG(x, ...) printf(x, __VA_ARGS__) // Temporary, maybe I'll create a logging library for performance measurements
+
 struct Ray {
     glm::vec3 org;
     glm::vec3 dir;
-    float t;
 };
-
 
 // TODO: when a ray misses, try using an environment map (cube map) instead of computing colors
 glm::vec3 RayMiss(const Ray& ray) {
@@ -31,45 +31,58 @@ glm::vec3 RayMiss(const Ray& ray) {
  *  t^2 (D.D) + 2t(O.D) + (O.O) - r^2 = 0
  *  Solve for t using quadratic formula
 */
-glm::vec3 TraceRay(const Ray& ray, const glm::vec3& camPos) {
-    glm::vec3 circlePos(0.0f, 0.0f, 10.0f);
-    float radius = 4.0f;
+glm::vec3 TraceRay(const Ray& ray) {
+    glm::vec3 lightDir(-1.0f);
+    float lightIntensity = 0.5f;
 
-    glm::vec3 co = circlePos - camPos; // if the camera is moved somewhere offset the rendering as if the circle is in the origin
+    glm::vec3 circlePos(0.0f);
+    float radius = 0.5f;
+
+    glm::vec3 co = ray.org - circlePos; // if the camera is moved somewhere offset the rendering as if the circle is at the origin of the camera
     float a = glm::dot(ray.dir, ray.dir);
-    float b = 2.0f * glm::dot(ray.dir, co);
+    float b = 2.0f * glm::dot(co, ray.dir);
     float c = glm::dot(co, co) - radius*radius;
-    float disc = b*b - 4*a*c;
+    float discriminant = b*b - 4*a*c;
 
-    if (disc >= 0) {
-        return glm::vec3(255, 0, 0);
+    // (-b +- sqrt(b^2 - 4ac))/2a
+    if (discriminant < 0) {
+        return RayMiss(ray);
     }
 
-    return RayMiss(ray);
+    float t0 = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+    float t1 = (-b + glm::sqrt(discriminant)) / (2.0f * a);
+
+    glm::vec3 hit0 = ray.org + t0 * ray.dir;
+    glm::vec3 hit1 = ray.org + t1 * ray.dir;
+
+    // Diffuse Lighting
+    glm::vec3 hitNorm = glm::normalize(hit0 - circlePos);
+    float cosTerm = glm::dot(hitNorm, -lightDir);
+
+    return lightIntensity * cosTerm * glm::vec3(0, 0, 255);
 }
 
 int main() {
-    std::ios_base::sync_with_stdio(false);
-
     RT::Image image(1920, 1080, 4);
     glm::vec2 viewport(2*image.aspectRatio, 2.0f);
-    glm::vec3 cameraPos(0);
+    glm::vec3 cameraPos(0, 0, 1);
 
     for (int y = 0; y < image.height; y++) {
         for (int x = 0; x < image.width; x++) {
-            std::clog << "\rProgress: " << std::round(((float)y / image.height) * 100) << '%';
+            LOG("\rProgress: %i%%", (int)std::round(((float)y / image.height) * 100));
+
             glm::vec2 fragPos = { (float)x / (image.width - 1), (float)y / (image.height - 1) };
             fragPos = fragPos * 2.0f - 1.0f;
             fragPos *= viewport;
 
-            glm::vec3 rayDir = glm::vec3(fragPos.x, fragPos.y, 1.0f);
+            glm::vec3 rayDir = glm::vec3(fragPos.x, fragPos.y, -1.0f);
             Ray ray(cameraPos, rayDir);
 
-            glm::vec3 pixelColor = TraceRay(ray, cameraPos);
-            int pixelIdx = image.comps * (y * image.width + x);
+            glm::vec3 pixelColor = TraceRay(ray);
+            pixelColor = glm::clamp(pixelColor, glm::vec3(0), glm::vec3(255));
 
+            int pixelIdx = image.comps * (y * image.width + x);
             DrawPixel(image, pixelIdx, {pixelColor, 255});
-            std::clog << std::flush;
         }
     }
 
